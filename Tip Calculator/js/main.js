6224c583433e1c44d9ae7217f37769eacdb2d5ba
coding4freedom <chawkins2010@hotmail.com>
2023-12-08 21:09:43 -0600

    Tip Calculator updated logic for concertToDecimal and displayTip

--- Tip Calculator/js/main.js
+++ Tip Calculator/js/main.js
@@ -1,22 +1,25 @@
 
 //declarations
-let percentRadioGruop = document.getElementsByName("percent");
+// let percentRadioGroup = document.getElementsByName("percent");
+let percentRadioGroup = document.querySelector('input[name="percent"]:checked');
 let userInput = document.querySelector('#user_input').value;
 let USDollar = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
 })
-
+// document.querySelector('input[name="JTP"]:checked')  
 //data grabs
 document.querySelector('#total').addEventListener('click', displayTip);
 
 
 //functions
-function checkUserInput(){
+function checkUserInput(){ //fix this userinput error problem
     if(Number.isInteger(parseInt(userInput))){
         convertToDecimal()
+        console.log('checkUserInput if statement ran')
     } else{
-        return document.querySelector("#result").innerHTML = "You have not entered a valid input"
+        console.log('checkUserInput else statement ran')
+        return document.querySelector("#result").innerHTML = "You have not entered a valid input";
     }
 }
 
@@ -24,12 +27,16 @@ function convertToDecimal(){
     // get this finish getting this code to work
     // take the userinput and the checked radio button value and get total
     // return that total in usda money
-    return parseInt(userInput) * percentRadioGruop.checked
+     let cost = parseInt(userInput) * percentRadioGroup.parseInt(value);
+     console.log(`convertToDecimal function ran cost is ${cost}`)
+     return USDollar.format(cost);
 }
 
 function displayTip(){
-    if(percentRadioGruop.checked){
-
+    if(percentRadioGroup.checked){
+        console.log('displayTip if statement ran')
+        return document.querySelector("#result").innerHTML = `${checkUserInput()}`
     }
+    console.log('displayTip ran outside of if statment')
 }
  
