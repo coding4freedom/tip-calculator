2d1052fc879d3313731c16d4c853cfc6e652ba8e
coding4freedom <chawkins2010@hotmail.com>
2023-12-10 18:17:45 -0600

    Tip Calculator update, fixed function arguments and parameters for user input, updated percent formula

--- Tip Calculator/js/main.js
+++ Tip Calculator/js/main.js
@@ -1,41 +1,45 @@
 
-//declarations
+// declarations
 // let percentRadioGroup = document.getElementsByName("percent");
 let percentRadioGroup = document.querySelector('input[name="percent"]:checked');
-let userInput = document.querySelector('#user_input').value;
+// let userInput = document.querySelector('#user_input').value;
 let USDollar = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
 })
 // document.querySelector('input[name="JTP"]:checked')  
-//data grabs
+// data grabs
 document.querySelector('#total').addEventListener('click', displayTip);
 
 
 //functions
-function checkUserInput(){ //fix this userinput error problem
-    if(Number.isInteger(parseInt(userInput))){
-        convertToDecimal()
+function checkUserInput(user){ //fix this userinput error problem
+    if(Number.isInteger(parseInt(user))){
+        convertToDecimal(user)
         console.log('checkUserInput if statement ran')
     } else{
         console.log('checkUserInput else statement ran')
+        console.log(user)
         return document.querySelector("#result").innerHTML = "You have not entered a valid input";
     }
 }
 
-function convertToDecimal(){
+function convertToDecimal(price){
     // get this finish getting this code to work
     // take the userinput and the checked radio button value and get total
     // return that total in usda money
-     let cost = parseInt(userInput) * percentRadioGroup.parseInt(value);
+     let cost = (parseInt(price)/100) * percentRadioGroup.value;
      console.log(`convertToDecimal function ran cost is ${cost}`)
+     console.log(USDollar.format(cost))
      return USDollar.format(cost);
 }
 
 function displayTip(){
+    let userInput = document.querySelector('#user_input').value;
     if(percentRadioGroup.checked){
         console.log('displayTip if statement ran')
-        return document.querySelector("#result").innerHTML = `${checkUserInput()}`
+        // fix this line output
+        return document.querySelector("#result").innerHTML = `${checkUserInput(userInput)}`
     }
     console.log('displayTip ran outside of if statment')
 }
