a22235e7c56420c596a5bb0ad39b0109ff191b5e
coding4freedom <chawkins2010@hotmail.com>
2024-01-06 21:19:52 -0600

    Tip Calulator added regex variable for user input

--- Tip Calculator/js/main.js
+++ Tip Calculator/js/main.js
@@ -4,7 +4,8 @@
 //let percentRadioGroup = document.querySelector('input[name="percent"]:checked');
 // let percentRadioGroup = document.querySelector('input[name="percent"]:checked');
 // let userInput = document.querySelector('#user_input').value;
-let unwanted = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','`','!','@','#','$','%','^','&','*','(',')','[',']']
+let unwanted = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','`','!','@','#','$','%','^','&','*','(',')','[',']'];
+const re = /([0-9]{1,3}\,[0-9]{1,3}+[.]{0,1}[0-9]{0,2}|[0-9]{1,3}\,[0-9]{1,3}|[0-9]{1,3}+[.]{0,1}[0-9]{0,2})/;
 let USDollar = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
@@ -55,7 +56,8 @@ function displayTip(){
 // fix line 20 user input is taking extra inputs like letters and symbols
 // working on regex to filter user input for money here is reges so far
 // [0-9]{1,3}\,[0-9]{1,3}+[.]{0,1}[0-9]{1,2}|[0-9]{1,3}\,[0-9]{1,3}
-// need to fix so it can account for money under 1,000 with out decimal
+// regex that is working
+/* ([0-9]{1,3}\,[0-9]{1,3}+[.]{0,1}[0-9]{0,2}|[0-9]{1,3}\,[0-9]{1,3}|[0-9]{1,3}+[.]{0,1}[0-9]{0,2}) */
 // need to fix userinput so that money decimal is accepted from user input (C)
 // way to fix issue change line 32 where the result of the calculation is .toFixed(2) (C)
 // remove the parseInt on that line, then you should be able to  (C)
