f23e444b70198a1b308884ea0a00d520867e447d
coding4freedom <chawkins2010@hotmail.com>
2024-02-19 21:11:36 -0600

    Stop Buck Gun class method generation

--- Stop Buck/js/main.js
+++ Stop Buck/js/main.js
@@ -6,9 +6,25 @@ class Guns {
         this.magSize = magSize
         this.attachments = attachments
         this.damage = damage
+
+        
     }
 
     register() {
         console.log(`You have register a ${this.type} ${this.name} with a magazine size of ${this.magSize}`)
     }
+
+    getTargetDmgScore(){
+        return Math.floor(Math.random() * (100 - 20 + 1))+ 20;
+    }
+
+    takeShot(){
+        return this.getTargetDmgScore() - this.damage
+    }
 }
+
+/*
+    Sample code to test class objects
+*/
+
+//const citizen = new Guns('Mossberg', 'shotgun', ['scope'], 50) 
